8f135744ee857594134cd309327f6b9303c0f895
Aloereed <58137529+Aloereed@users.noreply.github.com>
2023-03-09 13:44:22 +0800

    Update generation.py

--- llama/generation.py
+++ llama/generation.py
@@ -1,10 +1,3 @@
-'''
-Author: HU Zheng
-Date: 2023-03-08 10:17:27
-LastEditors: HU Zheng
-LastEditTime: 2023-03-08 20:51:55
-Description: file content
-'''
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # This software may be used and distributed according to the terms of the GNU General Public License version 3.
 
